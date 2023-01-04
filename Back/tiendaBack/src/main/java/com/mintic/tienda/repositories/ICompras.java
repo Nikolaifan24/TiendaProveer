0f80a1369775da3c73b00d0771d20ab1f9529f57
Nikolaifan24 <nikolaifan36@gmail.com>
2023-01-04 03:12:44 -0500

    consultas compras

--- Back/tiendaBack/src/main/java/com/mintic/tienda/repositories/ICompras.java
+++ Back/tiendaBack/src/main/java/com/mintic/tienda/repositories/ICompras.java
@@ -18,9 +18,10 @@ public interface ICompras extends CrudRepository<Compras, Long>{
 	@Query(value = "SELECT * FROM Compras p where p.FechaCompra=:FechaCompra", nativeQuery = true)
 	Compras buscarComprasPorFecha(@Param("FechaCompra") String FechaCompra);
 
-	@Query(value = "SELECT * FROM Compras p INNER JOIN Proveedor c ON c.IDProveedor = p.IDProveedor WHERE c.nombreProveedor= :nombreProveedor", nativeQuery = true)
+	@Query(value = "SELECT * FROM Compras p INNER JOIN Proveedores c ON c.IDProveedor = p.IDProveedor WHERE c.nombreProveedor= :nombreProveedor", nativeQuery = true)
 	List<Compras> buscarCompraPorNombreProveedor(@Param("nombreProveedor") String nombreProveedor);
 
 	@Query(value = "SELECT * FROM compras c INNER JOIN detalle_compra d ON c.IDCompras = d.IDCompra INNER JOIN productos p ON d.IDProducto = p.IDProductos WHERE p.nombreProducto = :nombreProducto", nativeQuery = true)
 	List<Compras> buscarCompraPorProductos(@Param("nombreProducto") String nombreProducto);
+
 }
